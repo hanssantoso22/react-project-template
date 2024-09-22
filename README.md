27bdd793812a353b1c6c4a75d14b54bf60cd5caf
Laurensius Hans Santoso <laurensiushans@gmail.com>
2024-09-22 22:00:11 +0800

    :sparkles: Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# React Project Template
+This is a template for React projects, with Babel and Webpack. This doesn't use create-react-app which is usually heavyweight.
+Sources:
+1. Install basic libraries: https://dev.to/ivadyhabimana/how-to-create-a-react-app-without-using-create-react-app-a-step-by-step-guide-30nl
+2. Install CSS loader (so that the project can parse CSS files): https://blog.jakoblind.no/css-modules-webpack/
+
+## How to use it
+1. Clone/download the repo
+2. Disconnect the project with git (delete `.git` directory)
+3. Rename the project directory
+4. `npm install` to install the necessary packages
+5. `npm start` to run the application 
+6. Push the repo to remote (https://docs.github.com/en/migrations/importing-source-code/using-the-command-line-to-import-source-code/adding-locally-hosted-code-to-github)
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+  },
+  target: 'web',
+  devServer: {
+    port: '5000',
+    static: {
+      directory: path.join(__dirname, 'public')
+    },
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.css'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, 
+        exclude: /node_modules/, 
+        use: 'babel-loader', 
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /\.module\.css$/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html')
+    })
+  ]
+};
